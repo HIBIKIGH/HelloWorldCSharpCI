5d9fb2347e462221ca61af82712ebbd38f37c985
HIBIKI <hibikifujimori@gmail.com>
2023-02-13 17:05:46 +0900

    テストメソッドの実装

--- HelloWorldCSharpCITest/UnitTest1.cs
+++ HelloWorldCSharpCITest/UnitTest1.cs
@@ -3,10 +3,20 @@ namespace HelloWorldCSharpCITest
     [TestClass]
     public class UnitTest1
     {
+        private const string Expected = "Hello World of C# CI !";
+        //private const string Expected = "Hello Unexpected World...";
+
         [TestMethod]
         public void TestMethod1()
         {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                HelloWorld.Program.Main();
 
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
         }
     }
 }
