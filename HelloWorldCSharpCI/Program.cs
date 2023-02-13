1db7db45b0cc26e87889f0ca7c7acb72a59b0505
HIBIKI <hibikifujimori@gmail.com>
2023-02-13 16:53:37 +0900

    メインクラスの追加。

--- HelloWorldCSharpCI/Program.cs
+++ HelloWorldCSharpCI/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World of C# CI !");
+        }
+    }
+}
